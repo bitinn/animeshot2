fabd833c7dd40fc79f04d009a916fa8e25f54535
David Frank <bitinn@gmail.com>
2018-10-06 00:28:27 +0800

    testing simple oauth flow

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const koaViews = require('koa-views');
 const koaLogger = require('koa-logger');
 const koaStatic = require('koa-static');
 const koaRouter = require('koa-trie-router');
+const koaSession = require('koa-session');
+const koaMount = require('koa-mount');
 
 const openrecord = require('openrecord/store/sqlite3');
+const grant = require('grant-koa');
+
+const settings = require('./animeshot.json');
 
 // define database
 
@@ -23,7 +28,7 @@ const db = new openrecord({
 
 const router = new koaRouter();
 
-router.get('/', async function (ctx) {
+router.get('/', async (ctx) => {
   await db.ready();
   const userModel = db.Model('users');
   const user = await userModel.find(1);
@@ -35,6 +40,10 @@ router.get('/', async function (ctx) {
   await ctx.render('index', data);
 });
 
+router.get(settings.oauth.server.callback, async (ctx) => {
+  ctx.body = ctx.session;
+});
+
 // define app
 
 const app = new koa();
@@ -44,6 +53,15 @@ app.use(koaStatic(__dirname + '/public'));
 app.use(koaViews(__dirname + '/views', {
   extension: 'pug'
 }));
+
+app.keys = settings.cookie.keys;
+app.use(koaSession({
+  key: settings.cookie.name,
+  maxAge: settings.cookie.maxAge,
+  signed: settings.cookie.signed
+}, app));
+
+app.use(koaMount(grant(settings.oauth)));
 app.use(router.middleware());
 
-app.listen(3000);
+app.listen(settings.site.port);
