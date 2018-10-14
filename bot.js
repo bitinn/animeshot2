78a9c0d0a28eae73f93a1fb6b62b05a2feb7adc6
David Frank <bitinn@gmail.com>
2018-10-14 20:46:33 +0800

    disable cache busting

--- bot.js
+++ bot.js
@@ -99,8 +99,8 @@ bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
     return {
       type: 'photo',
       id: shot.hash,
-      photo_url: settings.site.meta.base_url + "/uploads/" + shot.hash.substring(shot.hash.length - 2) + "/" + shot.hash + ".1080p.jpg?" + Date.now(),
-      thumb_url: settings.site.meta.base_url + "/uploads/" + shot.hash.substring(shot.hash.length - 2) + "/" + shot.hash + ".720p.jpg?" + Date.now(),
+      photo_url: settings.site.meta.base_url + "/uploads/" + shot.hash.substring(shot.hash.length - 2) + "/" + shot.hash + ".1080p.jpg",
+      thumb_url: settings.site.meta.base_url + "/uploads/" + shot.hash.substring(shot.hash.length - 2) + "/" + shot.hash + ".720p.jpg",
       photo_width: 1920,
       photo_height: 1080,
       caption: shot.text
